cffedb4a50e4b9b10904ed08e442acc39ca20933
Melih Alcik <105825418+darthshadoww@users.noreply.github.com>
2024-03-22 22:53:41 +0300

    First Changes

--- app.py
+++ app.py
@@ -18,3 +18,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+#Hello
--- models.py
+++ models.py
@@ -72,3 +72,4 @@ def index():
 # Run the Flask application
 if __name__ == '__main__':
     app.run(debug=True)
+#domo
